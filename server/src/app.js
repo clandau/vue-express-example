4b7936e4d70a133b7a71078b9c3280145eda70d1
courtney l <clandau@learnics.com>
2020-11-07 18:36:36 -0500

    server app

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const app = express();
+app.use(morgan("combined"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/status", (req, res) => {
-  res.send("hello world")
-})
+  res.send("hello world");
+});
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081);
